4e09c47ade8344cb014e2fecf571f3a1e66b215c
RealWorldd <jinpark99@naver.com>
2022-10-10 15:16:27 +0900

    fix: @PropertySouce 제거

--- src/main/java/teamproject1/letsdoit/LetsDoItApplication.java
+++ src/main/java/teamproject1/letsdoit/LetsDoItApplication.java
@@ -1,13 +1,10 @@
 package teamproject1.letsdoit;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@PropertySource(value = {"classpath:application-security.properties"})
 public class LetsDoItApplication {
 
 	public static void main(String[] args) {
