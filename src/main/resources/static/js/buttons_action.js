0fa18ad75c14da3474cc14b344741dd60e6078cd
RealWorldd <jinpark99@naver.com>
2022-10-14 16:04:08 +0900

    feat: 로그인 화면 추가

---
+++ src/main/resources/static/js/buttons_action.js
@@ -0,0 +1,19 @@
+
+var btnNaver = document.getElementsByClassName('naverbutton');
+btnNaver[0].addEventListener('click', function(){
+    //click action
+    console.log('네이버 버튼이 클릭되었습니다.');
+});
+var btnKakao = document.getElementsByClassName('kakaobutton');
+btnKakao[0].addEventListener('click', function(){
+    //click action
+    console.log('카카오 버튼이 클릭되었습니다.');
+});
+/*
+container.addEventListener('mouseenter', function(){
+    console.log('마우스가 들어옴.');
+});
+container.addEventListener('mouseleave', function(){
+    console.log('마우스가 나감.');
+});
+*/
