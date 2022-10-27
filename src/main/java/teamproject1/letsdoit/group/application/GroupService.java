87c369f04b733462a5bac540812ed20816acc9ce
Park Sejin <95167215+sejineer@users.noreply.github.com>
2022-10-28 00:34:30 +0900

    feat: 참여, 모집중인 그룹데이터 추가 (#13)

--- src/main/java/teamproject1/letsdoit/group/application/GroupService.java
+++ src/main/java/teamproject1/letsdoit/group/application/GroupService.java
@@ -1,6 +1,7 @@
 package teamproject1.letsdoit.group.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import teamproject1.letsdoit.common.exception.advice.assertThat.DefaultAssert;
@@ -8,7 +9,9 @@ import teamproject1.letsdoit.group.domain.Group;
 import teamproject1.letsdoit.group.domain.repository.GroupRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -28,6 +31,22 @@ public class GroupService {
 
     public List<Group> findGroups() {
         return groupRepository.findAll();
+    }
+
+    public List<Group> findGroupsMemberJoined (String email) {
+        List<Group> result = groupRepository.findAll().stream()
+                .filter(group -> group.getPeopleList().stream().anyMatch(mail -> mail.equals(email)))
+                        .collect(Collectors.toList());
+        log.info(result.toString());
+        return result;
+    }
+
+    public List<Group> findGroupsMemberCreate (String email) {
+        List<Group> result = groupRepository.findAll().stream()
+                .filter(group -> group.getHostEmail().equals(email))
+                .collect(Collectors.toList());
+        log.info(result.toString());
+        return result;
     }
 
 
