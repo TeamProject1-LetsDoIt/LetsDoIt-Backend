1f6f5c53baed59a3d438e08e90d895c05113a4d8
RealWorldd <jinpark99@naver.com>
2022-11-28 10:24:26 +0900

    feat: 알람 최신순 정렬

--- src/main/java/teamproject1/letsdoit/notice/application/NoticeService.java
+++ src/main/java/teamproject1/letsdoit/notice/application/NoticeService.java
@@ -7,6 +7,7 @@ import teamproject1.letsdoit.notice.domain.Notice;
 import teamproject1.letsdoit.notice.domain.Type;
 import teamproject1.letsdoit.notice.domain.repository.NoticeRepository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,8 +18,10 @@ public class NoticeService {
     private final NoticeRepository noticeRepository;
 
     public List<Notice> getNotices(Member member) {
-        return noticeRepository.findAll().stream()
+        List<Notice> notices = noticeRepository.findAll().stream()
                 .filter(notice -> notice.getMember().equals(member) || notice.getType().equals(Type.ANNOUNCEMENT))
                 .collect(Collectors.toList());
+        Collections.reverse(notices);
+        return notices;
     }
 }
